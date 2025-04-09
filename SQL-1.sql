16a8dfc29d90e2cd8b7cfa8609f02208c673d0b6
Renjith Mathews <53080803+Renmat077@users.noreply.github.com>
2025-04-09 18:05:11 -0400

    Update and rename SQL-1 to SQL-1.sql

--- SQL-1.sql
+++ SQL-1.sql
@@ -1,14 +1,14 @@
-Problem-1
-Assume you're given a table Twitter tweet data, write a query to obtain a histogram of tweets posted per user in 2022. Output the tweet count per user as the bucket and the number of Twitter users who fall into that bucket.
+# Problem-1
+/*Assume you're given a table Twitter tweet data, write a query to obtain a histogram of tweets posted per user in 2022. Output the tweet count per user as the bucket and the number of Twitter users who fall into that bucket.
 In other words, group the users by the number of tweets they posted in 2022 and count the number of users in each group.
 tweets Table:
 Column Name	Type
 1. tweet_id	integer
 2. user_id	integer
 3. msg	string
-4. tweet_date	timestamp
+4. tweet_date	timestamp /*
 
-Solution:
+*Solution:
 
 SELECT tweet_count as tweet_bucket, count(*) as user_count
 FROM (SELECT user_id, count(*) as tweet_count
